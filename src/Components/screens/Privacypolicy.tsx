926c764e2312c452a349d36067303b6b6b412da7
PrathamRathore9123 <prathamrathore20032gmail.com>
2026-01-02 17:53:55 +0530

    small changes in privacy policy page

--- src/Components/screens/Privacypolicy.tsx
+++ src/Components/screens/Privacypolicy.tsx
@@ -14,7 +14,7 @@ With our Privacy Policy, we ensure your personal data remains confidential, secu
 
           <h3 className="text-xl font-medium mt-4 mb-2">Personal Information</h3>
           <p>We may collect personal data when you book a package, create an account, contact customer service, or interact with our services, including:</p>
-          <ul className="list-disc list-inside text-left mx-auto max-w-2xl">
+          <ul className="list-disc list-inside text-left  max-w-2xl">
             <li>Identity Data: Full name, date of birth, gender, passport/visa details</li>
             <li>Contact Details: Email address, phone number, mailing address</li>
             <li>Payment Information: Credit/debit details, billing address</li>
@@ -25,14 +25,14 @@ With our Privacy Policy, we ensure your personal data remains confidential, secu
 
           <h3 className="text-xl font-medium mt-4 mb-2">Non-Personal Information</h3>
           <p>We may gather non-personal data, such as:</p>
-          <ul className="list-disc list-inside text-left mx-auto max-w-2xl">
+          <ul className="list-disc list-inside  max-w-2xl">
             <li>Device and Technical Information: IP address, device ID, operating system, browser data</li>
             <li>Cookies and Tracking Data: Website usage data, browsing operation, interactions with our ads and services</li>
           </ul>
 
           <h2 className="text-2xl font-semibold mt-6 mb-2">How We Use Your Information</h2>
           <p>We use your data for various purposes, including:</p>
-          <ul className="list-disc list-inside text-left mx-auto max-w-2xl">
+          <ul className="list-disc list-inside max-w-2xl">
             <li>To process and manage bookings: Ensuring smooth travel arrangements, payment processing, and confirmations</li>
             <li>To provide customer support: Assisting with travel queries, cancellations, and refunds</li>
             <li>To enhance user experience: Personalizing recommendations and travel offers based on past behaviour.</li>
@@ -43,7 +43,7 @@ With our Privacy Policy, we ensure your personal data remains confidential, secu
 
           <h2 className="text-2xl font-semibold mt-6 mb-2">How We Share Your Information</h2>
           <p>We do not sell or rent your personal information. However, we may share it with third parties in the following circumstances:</p>
-          <ul className="list-disc list-inside text-left mx-auto max-w-2xl">
+          <ul className="list-disc list-inside max-w-2xl">
             <li>Travel Service Providers: Airlines, hotels, car rental agencies, tour operators, and third-party booking partners to facilitate your reservations</li>
             <li>Payment Processors: Banks, financial institutions, and fraud prevention agencies to complete transactions securely</li>
             <li>Legal and Regulatory Authorities: If required by law, government authorities, or to protect our legal rights</li>
@@ -52,7 +52,7 @@ With our Privacy Policy, we ensure your personal data remains confidential, secu
 
           <h2 className="text-2xl font-semibold mt-6 mb-2">How We Protect Your Information</h2>
           <p>We take data security seriously and implement measures to safeguard your information, including:</p>
-          <ul className="list-disc list-inside text-left mx-auto max-w-2xl">
+          <ul className="list-disc list-inside text-left  max-w-2xl">
             <li>Encryption and Secure Transactions: Payment data is encrypted using industry-standard security protocols</li>
             <li>Access Control: Restricted access to sensitive data, available only to authorized personnel</li>
             <li>Regular Security Audits: We monitor and update our security systems to prevent data breaches</li>
@@ -71,7 +71,7 @@ With our Privacy Policy, we ensure your personal data remains confidential, secu
 
           <h2 className="text-2xl font-semibold mt-6 mb-2">Cookies and Tracking Technologies</h2>
           <p>Our website uses cookies and similar technologies to enhance user experience and analyze website traffic.</p>
-          <ul className="list-disc list-inside text-left mx-auto max-w-2xl">
+          <ul className="list-disc list-inside text-left max-w-2xl">
             <li>Essential Cookies: Required for website functionality</li>
             <li>Analytics Cookies: Used to understand user behavior and improve services</li>
             <li>Marketing Cookies: Used for personalized advertising campaigns</li>
@@ -103,7 +103,7 @@ With our Privacy Policy, we ensure your personal data remains confidential, secu
           <p>This page represents the legal terms and conditions of Unravel Experiences. These Terms and Conditions ("Terms") govern your use of the website, the app, or any travelling experience. By using our services, you agree to abide by these Terms.</p>
 
           <h2 className="text-2xl font-semibold mt-6 mb-2">Booking and Payments</h2>
-          <ul className="list-disc list-inside text-left mx-auto max-w-2xl">
+          <ul className="list-disc list-inside text-left max-w-2xl">
             <li>All bookings are subject to availability.</li>
             <li>Full or partial payment may be required at the time of booking.</li>
             <li>Payments can be made via credit/debit cards, UPI, bank transfers, or other accepted methods.</li>
@@ -111,7 +111,7 @@ With our Privacy Policy, we ensure your personal data remains confidential, secu
           </ul>
 
           <h2 className="text-2xl font-semibold mt-6 mb-2">Cancellation and Refund Policy</h2>
-          <ul className="list-disc list-inside text-left mx-auto max-w-2xl">
+          <ul className="list-disc list-inside text-left max-w-2xl">
             <li>Full cancellations are allowed only if requested at least 30 days before departure. After this, eligible refunds may be granted based on the cancellation policies of the booked services. Our team will provide exact details at the time of booking. For more information, contact our support team.</li>
             <li>Cancellations made less than 30 days before departure may not be eligible for a refund.</li>
             <li>Refund processing time may vary based on the payment method.</li>
@@ -119,31 +119,31 @@ With our Privacy Policy, we ensure your personal data remains confidential, secu
           </ul>
 
           <h2 className="text-2xl font-semibold mt-6 mb-2">Travel Documents and Requirements</h2>
-          <ul className="list-disc list-inside text-left mx-auto max-w-2xl">
+          <ul className="list-disc list-inside text-left  max-w-2xl">
             <li>Customers are responsible for ensuring they have valid passports, visas, and other crucial travel documents.</li>
             <li>Unravel Experiences is not liable for any denial of entry due to incomplete or unauthorised documents.</li>
           </ul>
 
           <h2 className="text-2xl font-semibold mt-6 mb-2">Health and Safety</h2>
-          <ul className="list-disc list-inside text-left mx-auto max-w-2xl">
+          <ul className="list-disc list-inside text-left  max-w-2xl">
             <li>Travelers are responsible for meeting health and required medical procedures.</li>
             <li>Unravel Experiences is not liable for any health-related issues during the trip.</li>
           </ul>
 
           <h2 className="text-2xl font-semibold mt-6 mb-2">Itinerary Changes</h2>
-          <ul className="list-disc list-inside text-left mx-auto max-w-2xl">
+          <ul className="list-disc list-inside text-left  max-w-2xl">
             <li>Unravel Experiences reserves the right to modify trip itineraries due to weather conditions, safety concerns, or any operational reasons.</li>
             <li>Alternative arrangements will be made wherever possible.</li>
           </ul>
 
           <h2 className="text-2xl font-semibold mt-6 mb-2">Liability and Responsibilities</h2>
-          <ul className="list-disc list-inside text-left mx-auto max-w-2xl">
+          <ul className="list-disc list-inside text-left max-w-2xl">
             <li>Unravel Experiences acts as a mediator between customers and service providers (hotels, airlines, transport, etc.) and is not liable for any loss, injury, or damage caused by third parties.</li>
             <li>Customers participate in adventurous activities at their own risk.</li>
           </ul>
 
           <h2 className="text-2xl font-semibold mt-6 mb-2">Code of Conduct</h2>
-          <ul className="list-disc list-inside text-left mx-auto max-w-2xl">
+          <ul className="list-disc list-inside text-left  max-w-2xl">
             <li>Travelers must respect local customs, laws, and fellow travelers.</li>
             <li>Any misconduct may result in removal from the trip without a refund.</li>
           </ul>
@@ -155,7 +155,7 @@ With our Privacy Policy, we ensure your personal data remains confidential, secu
           <p>We understand the sudden shift in your trip plans; however, at Unravel Experience, we have a certain cancellation policy. Below is our cancellation and refund policy to ensure transparency and fairness.</p>
 
           <h2 className="text-2xl font-semibold mt-6 mb-2">Cancellation Policy</h2>
-          <ul className="list-disc list-inside text-left mx-auto max-w-2xl">
+          <ul className="list-disc list-inside text-left max-w-2xl">
             <li>Cancellations must be requested in writing via email at [Email Address] or through our customer support.</li>
             <li>Cancellations made 30 days or more before the departure date are eligible for a refund as per our refund policy.</li>
             <li>Cancellations made less than 30 days before departure are non-refundable.</li>
@@ -163,7 +163,7 @@ With our Privacy Policy, we ensure your personal data remains confidential, secu
           </ul>
 
           <h2 className="text-2xl font-semibold mt-6 mb-2">Refund Policy</h2>
-          <ul className="list-disc list-inside text-left mx-auto max-w-2xl">
+          <ul className="list-disc list-inside text-left max-w-2xl">
             <li>Refunds are only applicable for cancellations made at least 30 days before departure.</li>
             <li>The refund amount will be subject to applicable cancellation fees and deductions, which may include administrative fees, service charges, and third-party costs (hotels, airlines, etc.).</li>
             <li>Refunds will be processed within 7-14 business days after the cancellation request is approved.</li>
@@ -171,13 +171,13 @@ With our Privacy Policy, we ensure your personal data remains confidential, secu
           </ul>
 
           <h2 className="text-2xl font-semibold mt-6 mb-2">Changes and Rescheduling</h2>
-          <ul className="list-disc list-inside text-left mx-auto max-w-2xl">
+          <ul className="list-disc list-inside text-left  max-w-2xl">
             <li>If you wish to modify your booking, requests must be made at least 30 days before departure, and changes will be subject to availability and additional charges.</li>
             <li>Rescheduling within 30 days of departure may not be possible.</li>
           </ul>
 
           <h2 className="text-2xl font-semibold mt-6 mb-2">Trip Cancellations by Unravel Experiences</h2>
-          <ul className="list-disc list-inside text-left mx-auto max-w-2xl">
+          <ul className="list-disc list-inside text-left  max-w-2xl">
             <li>In the rare event that we need to cancel a trip due to unforeseen circumstances (such as natural disasters, government restrictions, or safety concerns), we will offer you:</li>
             <li>A full refund of your trip</li>
             <li>Unravel Experiences is not responsible for additional costs incurred by travelers, such as visa fees, flight tickets, or accommodation booked separately.</li>
